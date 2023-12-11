2c687edb6e32eeb9b7a38115c18eb2c504b737b7
Francisco González Ibáñez <fragonib@gmail.com>
2023-12-11 14:47:13 +0100

    only verify seed ranges

--- src/AoC2023/Day5Part2.hs
+++ src/AoC2023/Day5Part2.hs
@@ -8,63 +8,107 @@ import Data.List (sortBy)
 import Data.List.Split (splitOn)
 
 data Mapping = Mapping
-  { destinationRangeStart :: Int,
-    souceRangeStart :: Int,
-    rangeLenght :: Int
+  { mappingDestination :: Int,
+    mappingSource :: Int,
+    mappingLength :: Int
   }
   deriving (Show, Eq)
 
 type Almanac = [[Mapping]]
 
+data SeedRange = SeedRange
+  { seedStart :: Int,
+    seedLength :: Int
+  }
+  deriving (Show, Eq)
+
 solve :: [String] -> Int
-solve lines = minimum locations
+solve lines =
+  let locationsOfOnleSeedStarts = map (location almanac . seedStart) splittedSeedRanges
+   in minimum locationsOfOnleSeedStarts
   where
-    locations = map (location almanac) seeds
-    seeds = parseSeeds $ head $ head sections
-    almanac = map (sortBy byStartRange) (parseAlmanac $ drop 1 sections)
-    sections = splitInSections lines
+    splittedSeedRanges = splitSeedRanges almanac seedRanges
+    (seedRanges, almanac) = parseProblemLines lines
+
+splitSeedRanges :: Almanac -> [SeedRange] -> [SeedRange]
+splitSeedRanges almanac = concatMap (splitSeedRange almanac)
+
+splitSeedRange :: Almanac -> SeedRange -> [SeedRange]
+splitSeedRange almanac seed@(SeedRange start length) =
+  if maxRange == length
+    then [seed]
+    else SeedRange start maxRange : splitSeedRange almanac nextSeedRange
+  where
+    nextSeedRange = SeedRange (start + maxRange) (length - maxRange)
+    maxRange = seedLength $ foldl (flip maxSeedRange) seed almanac
+
+maxSeedRange :: [Mapping] -> SeedRange -> SeedRange
+maxSeedRange mappings (SeedRange sStart sLength) =
+  let foundMapping = mappingBinarySearch isInMapping sStart mappings
+   in case foundMapping of
+        Nothing -> SeedRange sStart maxRange
+          where
+            maxRange = case nextMapping of
+              Nothing -> sLength
+              Just mapping -> min sLength (mappingSource mapping - sStart)
+            nextMapping = head' $ filter ((> sStart) . mappingSource) mappings
+        Just (Mapping destination mSource mLength) ->
+          SeedRange
+            (applyMapping foundMapping sStart)
+            (min sLength (mLength - (sStart - mSource)))
 
 location :: Almanac -> Int -> Int
-location almanac seed = foldl (flip infer) seed almanac
+location almanac seed = foldl (flip lookMappingAndApply) seed almanac
 
-infer :: [Mapping] -> Int -> Int
-infer mappings targetNum =
-  let foundMapping = binaryMappingSearch isInMapping targetNum mappings
-   in case foundMapping of
-        Nothing -> targetNum
-        Just (Mapping destination source _) -> destination + (targetNum - source)
+lookMappingAndApply :: [Mapping] -> Int -> Int
+lookMappingAndApply mappings targetNum = applyMapping foundMapping targetNum
+  where
+    foundMapping = mappingBinarySearch isInMapping targetNum mappings
+
+applyMapping :: Maybe Mapping -> Int -> Int
+applyMapping Nothing targetNum = targetNum
+applyMapping (Just (Mapping destination source _)) targetNum =
+  destination + (targetNum - source)
 
 isInMapping :: Mapping -> Int -> Bool
 isInMapping (Mapping destination source range) targetNum =
   targetNum >= source && targetNum < (source + range)
 
-byStartRange :: Mapping -> Mapping -> Ordering
-byStartRange = compare `on` souceRangeStart
+compareByStartRange :: Mapping -> Mapping -> Ordering
+compareByStartRange = compare `on` mappingSource
 
-binaryMappingSearch :: (Mapping -> Int -> Bool) -> Int -> [Mapping] -> Maybe Mapping
-binaryMappingSearch _ _ [] = Nothing
-binaryMappingSearch matcher elem mappings
+mappingBinarySearch :: (Mapping -> Int -> Bool) -> Int -> [Mapping] -> Maybe Mapping
+mappingBinarySearch _ _ [] = Nothing
+mappingBinarySearch matcher elem mappings
   | matcher mapping elem = Just mapping
-  | elem < source = binaryMappingSearch matcher elem smaller
-  | otherwise = binaryMappingSearch matcher elem bigger
+  | elem < source = mappingBinarySearch matcher elem smaller
+  | otherwise = mappingBinarySearch matcher elem bigger
   where
     Mapping _ source _ = mapping
     (smaller, mapping : bigger) = splitAt index mappings
     index = length mappings `quot` 2
 
+head' :: [a] -> Maybe a
+head' [] = Nothing
+head' (x : xs) = Just x
+
 -- Parsing
+
+parseProblemLines :: [String] -> ([SeedRange], Almanac)
+parseProblemLines lines = (seedRanges, almanac)
+  where
+    seedRanges = parseSeeds $ head $ head sections
+    almanac = map (sortBy compareByStartRange) (parseAlmanac $ drop 1 sections)
+    sections = splitInSections lines
 
 splitInSections :: [String] -> [[String]]
 splitInSections = splitOn [""]
 
-parseSeeds :: String -> [Int]
-parseSeeds line = seedsRange >>= expandSeedRange
+parseSeeds :: String -> [SeedRange]
+parseSeeds line = map (uncurry SeedRange) seedsPairs
   where
-    seedsRange = window seeds
+    seedsPairs = window seeds
     seeds = (map read . drop 1 . words) line
-
-expandSeedRange :: (Enum a, Num a) => (a, a) -> [a]
-expandSeedRange (num, amount) = map (num +) [0 .. amount - 1]
 
 window :: [a] -> [(a, a)]
 window [] = []
